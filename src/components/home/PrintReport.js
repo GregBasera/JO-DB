f4d40f8cc0aeedefe8cff48a8a3c978294d89829
GregBasera <basera.gg@gmail.com>
2021-07-19 17:00:54 +0800

    starting report generation function

--- src/components/home/PrintReport.js
+++ src/components/home/PrintReport.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-import { Dialog, DialogContent, Typography, DialogActions, Button, IconButton } from "@material-ui/core";
+import { Dialog, DialogContent, Typography, DialogActions, Button, IconButton, Grid } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
+import { Gdropdown, Gtextfield } from "../shared/FormElements";
 import CloseIcon from "@material-ui/icons/Close";
 
 const styles = (theme) => ({
@@ -31,19 +33,41 @@ const DialogTitle = withStyles(styles)((props) => {
 });
 
 export default function PrintReport({ open, handleClose, data }) {
-  console.log(data);
   return (
     <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
       <DialogTitle id="customized-dialog-title" onClose={handleClose}>
         Print Report
       </DialogTitle>
       <DialogContent dividers>
-        <Typography>{JSON.stringify(data)}</Typography>
+        {/* <Alert severity="warning">This is an error alert — check it out!</Alert> */}
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Gdropdown name="office_assignment" label="Office Assignment" menuItems={[]} />
+          </Grid>
+          <Grid item xs={6}>
+            <Gtextfield
+              type="date"
+              id="ep_start"
+              label="Employment Period: Start"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Gtextfield
+              type="date"
+              id="ep_end"
+              label="Employment Period: End"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </Grid>
+        </Grid>
       </DialogContent>
       <DialogActions>
-        <Button autoFocus onClick={handleClose} color="primary">
-          Save changes
-        </Button>
+        <Button onClick={handleClose}>Save changes</Button>
       </DialogActions>
     </Dialog>
   );
