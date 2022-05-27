99a9cf6d730f41a6c65afc7f87c9488641e5de43
GregBasera <basera.gg@gmail.com>
2022-05-27 11:49:09 +0800

    dynamic baseurl for database

--- src/components/shared/endpoints.js
+++ src/components/shared/endpoints.js
@@ -1,4 +1,5 @@
-var baseurl = "http://localhost:1337";
+// assumes that the database (strapi) is in the same IP/hostname as the app
+var baseurl = `http://${window.location.hostname}:1337`;
 
 const Auth = baseurl + "/auth/local";
 const Register = baseurl + "/auth/local/register";
