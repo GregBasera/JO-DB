7d314006f52ecd338bf6a6225e55fdd95bfb3d9e
GregBasera <basera.gg@gmail.com>
2022-05-27 11:47:41 +0800

    centralized options for dropdowns

---
+++ src/components/shared/sharedVariables.js
@@ -0,0 +1,26 @@
+export const empStatus = [
+  { _id: "Newly Hired", value: "Newly Hired" },
+  { _id: "Re-Appointed", value: "Re-Appointed" },
+  { _id: "Resigned", value: "Resigned" },
+  { _id: "Terminated", value: "Terminated" },
+  { _id: "Seperated", value: "Seperated" },
+  { _id: "Promoted to Permanent", value: "Promoted to Permanent" },
+];
+
+export const genFunction = [
+  { _id: "IT Service", value: "IT Service" },
+  { _id: "Teaching Service", value: "Teaching Service" },
+  { _id: "Health and Allied Service", value: "Health and Allied Service" },
+  { _id: "Technical Service", value: "Technical Service" },
+  { _id: "Clerical Service", value: "Clerical Service" },
+  { _id: "Janitorial Service", value: "Janitorial Service" },
+  { _id: "Security Service", value: "Security Service" },
+  { _id: "Trade and Crafts / Laborer", value: "Trade and Crafts / Laborer" },
+  { _id: "Others", value: "Others" },
+];
+
+export const appointStatus = [
+  { _id: "Job Order", value: "Job Order" },
+  { _id: "Contractual", value: "Contractual" },
+  { _id: "Locally Funded", value: "Locally Funded" },
+];
