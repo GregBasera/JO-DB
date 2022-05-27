620474e19a61261335991ebda6843e831c8abc78
GregBasera <basera.gg@gmail.com>
2022-05-27 11:43:12 +0800

    replaced personnel cards with lists v1.1

---
+++ src/components/home/EmployeeList.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import moment from "moment";
+import { IconButton, Typography, Box, Grid } from "@material-ui/core";
+import MoreInfoDialog from "./Dialogs/MoreInfoDialog";
+import AddAppointDialog from "./Dialogs/AddAppointDialog";
+import EditAppointsDialog from "./Dialogs/EditAppointsDialog";
+import DeletePersonelDialog from "./Dialogs/DeletePersonelDialog";
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import TokenVerifier from "../shared/TokenVerifier";
+
+function statusNode(value) {
+  // separated this for cleaner render function
+  switch (value.toLowerCase()) {
+    case "new":
+    case "re-appointed":
+      return (
+        <b title="Status" style={{ color: "green" }}>
+          {value}
+        </b>
+      );
+    case "resigned":
+    case "terminated":
+      return (
+        <b title="Status" style={{ color: "red" }}>
+          {value}
+        </b>
+      );
+    case "promoted":
+      return (
+        <b title="Status" style={{ color: "orange" }}>
+          {value}
+        </b>
+      );
+    default:
+      return <b title="Status">{value}</b>;
+  }
+}
+
+export default function EmployeeList({ data, deleteOne, newHistory, offices, funding }) {
+  let { designation, ep_start, ep_end, office_assignment, status } = data.service_history[0];
+
+  const [showMoreOpen, setShowMoreOpen] = useState(false);
+  const [addDialog, setAddDialog] = useState(false);
+  const [editDialog, setEditDialog] = useState(false);
+  const [deleteDialog, setDeleteDialog] = useState(false);
+
+  return (
+    <React.Fragment>
+      <Grid container spacing={1} style={{ backgroundColor: "white", marginBottom: "1px" }}>
+        <Grid item xs={3} style={{ borderLeft: "1px lightgray solid" }}>
+          <Typography>{data.name}</Typography>
+        </Grid>
+        <Grid item xs={2} style={{ borderLeft: "1px lightgray solid" }}>
+          <Typography>{designation}</Typography>
+        </Grid>
+        {/* <Grid item xs={1}>
+          <Typography>{rate_per_day}</Typography>
+        </Grid> */}
+        <Grid item xs={2} style={{ borderLeft: "1px lightgray solid" }}>
+          <Typography>{moment(ep_start).format("MMM DD YYYY") + " - " + moment(ep_end).format("MMM DD YYYY")}</Typography>
+        </Grid>
+        <Grid item xs={3} style={{ borderLeft: "1px lightgray solid" }}>
+          <Typography>{office_assignment}</Typography>
+        </Grid>
+        <Grid item xs={1} style={{ borderLeft: "1px lightgray solid" }}>
+          <Typography>{statusNode(status)}</Typography>
+        </Grid>
+        <Grid item xs={1} style={{ backgroundColor: "skyblue" }}>
+          <Box display="flex" flexDirection="row-reverse" style={{ width: "100%" }}>
+            <IconButton size="small" title="Delete Personel Record" onClick={() => setDeleteDialog(true)} style={TokenVerifier(2) ? { display: "inline" } : { display: "none" }}>
+              <DeleteIcon />
+            </IconButton>
+            <IconButton size="small" title="Edit Existing Appointments" onClick={() => setEditDialog(true)} style={TokenVerifier(1) ? { display: "inline" } : { display: "none" }}>
+              <EditIcon />
+            </IconButton>
+            <IconButton size="small" title="Add Lastest Appointment" onClick={() => setAddDialog(true)} style={TokenVerifier(1) ? { display: "inline" } : { display: "none" }}>
+              <AddIcon />
+            </IconButton>
+            <IconButton size="small" title="More" onClick={() => setShowMoreOpen(true)}>
+              <MoreHorizIcon />
+            </IconButton>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <MoreInfoDialog handleClose={() => setShowMoreOpen(false)} open={showMoreOpen} data={data} />
+      <AddAppointDialog handleClose={() => setAddDialog(false)} open={addDialog} data={data} newHistory={newHistory} offices={offices} funding={funding} />
+      <EditAppointsDialog handleClose={() => setEditDialog(false)} open={editDialog} data={data} offices={offices} funding={funding} editCallback={newHistory} />
+      <DeletePersonelDialog handleClose={() => setDeleteDialog(false)} open={deleteDialog} data={{ name: data.name, id: data._id }} deleteOne={deleteOne} />
+    </React.Fragment>
+  );
+}
--- src/components/home/HomeBody.js
+++ src/components/home/HomeBody.js
@@ -1,15 +1,16 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Typography, CircularProgress } from "@material-ui/core";
+import { Grid, Typography, CircularProgress, List, ListItem } from "@material-ui/core";
 import TopControls from "./TopControls";
-import PersonelCard from "./PersonelCard";
+// import PersonelCard from "./PersonelCard";
 import { initialize, onRecord, getOffices, getFundSources } from "./APIcalls";
+import EmployeeList from "./EmployeeList";
 
 export default function HomeBody() {
   const [data, setData] = useState([]);
   const [onRecordState, setonRecordState] = useState(null);
   const [offices, setOffices] = useState(null);
   const [funding, setFunding] = useState(null);
-  const [isDesc, setIsDesc] = useState(true);
+  const [isDesc, setIsDesc] = useState(false);
   useEffect(() => {
     initialize(isDesc ? "desc" : "asc", setData);
     getOffices(setOffices);
@@ -37,13 +38,39 @@ export default function HomeBody() {
     <React.Fragment>
       <TopControls append={appendData} setData={setData} data={data} sort={setIsDesc} isDesc={isDesc} />
 
-      <Grid container spacing={1} style={{ margin: "0px", backgroundColor: "#eeeeee" }}>
+      <Grid container spacing={1} style={{ backgroundColor: "#eeeeee", marginBottom: "1px" }}>
+        <Grid item xs={3}>
+          <Typography align="center">Employee Name</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography align="center">Position</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography align="center">Lastest Appoint</Typography>
+        </Grid>
+        <Grid item xs={3}>
+          <Typography align="center">Office/Department</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography align="center">Status</Typography>
+        </Grid>
+        <Grid item xs={1}>
+          <Typography align="center">Actions</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={1} style={{ padding: "0px", backgroundColor: "#eeeeee" }}>
         {data.length !== 0 ? (
-          data.map((personel) => (
-            <Grid key={personel._id} item xs={12} md={2}>
+          <List dense={true} disablePadding={true} style={{ width: "100%" }}>
+            {data.map((personel) => (
+              <ListItem>
+                <EmployeeList data={personel} deleteOne={popOneOut} newHistory={addNewAppoint} offices={offices} funding={funding} />
+              </ListItem>
+            ))}
+            {/* <Grid key={personel._id} item xs={12} md={2}>
               <PersonelCard data={personel} deleteOne={popOneOut} newHistory={addNewAppoint} offices={offices} funding={funding} />
-            </Grid>
-          ))
+            </Grid> */}
+          </List>
         ) : (
           <Typography>
             <CircularProgress />
