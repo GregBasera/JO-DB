63984c964eb5fad172d475d88a3b9d764e7834a2
GregBasera <basera.gg@gmail.com>
2022-05-27 11:44:05 +0800

    replaced popover with a dialog

--- src/components/home/TopControls.js
+++ src/components/home/TopControls.js
@@ -1,55 +1,43 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Box, IconButton, Popover, Button, ButtonGroup } from "@material-ui/core";
+import { Grid, Box, IconButton } from "@material-ui/core";
 import { Gtextfield, Gdropdown, Gbutton } from "../shared/FormElements";
-import AddPersonelDialog from "./AddPersonelDialog";
-import AddDepartmentDialog from "./AddDepartmentDialog";
+import AddPersonelDialog from "./Dialogs/AddPersonelDialog";
+import AddDepartmentDialog from "./Dialogs/AddDepartmentDialog";
 import { getOffices, getFundSources, search, filter } from "./APIcalls";
 import AddIcon from "@material-ui/icons/Add";
 import PrintIcon from "@material-ui/icons/Print";
 import SortByAlphaIcon from "@material-ui/icons/SortByAlpha";
 import TokenVerifier from "../shared/TokenVerifier";
+import PrintDialog from "./Dialogs/PrintDialog";
 
 export default function TopControls({ append, setData, data, sort, isDesc }) {
   const [addPersonelDialog, setAddPersonelDialog] = useState(false);
   const [addDepartmentDialog, setAddDepartmentDialog] = useState(false);
   const [addFundingDialog, setFundingDialog] = useState(false);
-  // const [print, setPrint] = useState(false);
   const [depts, setDepts] = useState([]);
   const [fundSources, setFundSources] = useState([]);
+  const [offAssign, setOffAssign] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(null);
+  const [showPrintDialog, setShowPrintDialog] = useState(false);
+
   useEffect(() => {
     getOffices(setDepts);
-  }, []);
-  useEffect(() => {
     getFundSources(setFundSources);
   }, []);
 
-  const [offAssign, setOffAssign] = useState(null);
   const handleOAchange = (e) => {
     setOffAssign(e.target.value);
     filter(["office_assignment", e.target.value], setData);
   };
-
-  const [searchTerm, setSearchTerm] = useState(null);
   const handleSearch = (e) => {
     if (e.key === "Enter") {
       search(searchTerm, setData);
     }
     setSearchTerm(e.target.value);
   };
-
-  const printAppReport = () => {
-    localStorage.setItem("forPrinting", JSON.stringify(data));
+  const showPriDialog = () => {
+    setShowPrintDialog(true);
   };
-
-  const [anchorEl, setAnchorEl] = useState(null);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
 
   return (
     <React.Fragment>
@@ -71,43 +59,20 @@ export default function TopControls({ append, setData, data, sort, isDesc }) {
             <Box display={TokenVerifier(2) ? "block" : "none"}>
               <Gbutton icon={<AddIcon />} size="medium" text="Funding" onClick={() => setFundingDialog(true)} />
             </Box>
-            <IconButton onClick={handleClick} style={TokenVerifier(1) ? { display: "block" } : { display: "none" }}>
+            <IconButton onClick={showPriDialog} style={TokenVerifier(1) ? { display: "block" } : { display: "none" }}>
               <PrintIcon />
             </IconButton>
 
             <IconButton onClick={() => sort(!isDesc)} color={isDesc ? "secondary" : "inherit"}>
               <SortByAlphaIcon />
             </IconButton>
-            <Popover
-              id={id}
-              open={open}
-              anchorEl={anchorEl}
-              onClose={handleClose}
-              elevation={2}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "center",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "center",
-              }}>
-              <ButtonGroup>
-                <Button href={`${document.location.origin}/JOappreport`} target="_blank" onClick={printAppReport}>
-                  Appointment Report
-                </Button>
-                <Button href={`${document.location.origin}/perinforeport`} target="_blank" onClick={printAppReport}>
-                  JO Information Report
-                </Button>
-              </ButtonGroup>
-            </Popover>
           </Box>
         </Grid>
       </Grid>
-
       <AddPersonelDialog open={addPersonelDialog} handleClose={() => setAddPersonelDialog(false)} depts={depts} fundSources={fundSources} append={append} />
       <AddDepartmentDialog open={addDepartmentDialog} handleClose={() => setAddDepartmentDialog(false)} mode="offices" />
       <AddDepartmentDialog open={addFundingDialog} handleClose={() => setFundingDialog(false)} mode="funding" />
+      <PrintDialog open={showPrintDialog} handleClose={() => setShowPrintDialog(false)} data={data} />
     </React.Fragment>
   );
 }
