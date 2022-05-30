1cebbd1f29bf64cd11c402954c024ed4255fd5aa
GregBasera <basera.gg@gmail.com>
2022-05-30 15:28:22 +0800

    service record changes

--- src/components/Reports/ServiceRecord.js
+++ src/components/Reports/ServiceRecord.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import MuiDialogTitle from "@material-ui/core/DialogTitle";
-import { Grid, Typography, Table, TableHead, TableRow, TableBody, Dialog, DialogContent, DialogActions, Button, FormControlLabel, Checkbox } from "@material-ui/core";
+import { Grid, Typography, Table, TableHead, TableRow, TableBody, Dialog, DialogContent, DialogActions, Button } from "@material-ui/core";
 import MuiTableCell from "@material-ui/core/TableCell";
 import moment from "moment";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import HRdocHeader from "./HRdocHeader";
+import { Gtextfield } from "../shared/FormElements";
 // import { Alert } from "@material-ui/lab";
 
 const TableCell = withStyles({
@@ -29,14 +30,24 @@ export default function ServiceRecord() {
   let { name, service_history, birthdate, birthplace, address, status } = useServiceRecord;
 
   const [open, setOpen] = useState(true);
-  const [isAbsent, setIsAbsent] = useState(false);
-  const checkChange = (e) => {
-    setIsAbsent(e.target.checked);
+  const [details, setDetails] = useState({
+    prepby: "[prepby]",
+    prepbyPosition: "[prepbyPosition]",
+    approvedby: "[attestby]",
+    approvedbyPosition: "[attestbyPosition]",
+  });
+
+  const signatoriesChanges = (e) => {
+    setDetails({ ...details, [e.target.id]: e.target.value });
   };
+  // const [isAbsent, setIsAbsent] = useState(false);
+  // const checkChange = (e) => {
+  //   setIsAbsent(e.target.checked);
+  // };
 
   return (
     <React.Fragment>
-      <FinishingDetails open={open} handleClose={() => setOpen(false)} isAbsent={isAbsent} isAbsentChange={checkChange} />
+      <FinishingDetails open={open} handleClose={() => setOpen(false)} details={details} changes={signatoriesChanges} />
       <HRdocHeader paperSize="a4" />
 
       <Grid container spacing={0}>
@@ -129,7 +140,7 @@ export default function ServiceRecord() {
                 return (
                   <TableRow key={index}>
                     <TableCell align="center">{moment(q.ep_start).format("DD-MMM-YYYY")}</TableCell>
-                    <TableCell align="center">{moment(q.ep_end).format("DD-MMM-YYYY")}</TableCell>
+                    <TableCell align="center">{moment(q.ep_end).isSameOrAfter(moment()) ? "to date" : moment(q.ep_end).format("DD-MMM-YYYY")}</TableCell>
                     <TableCell align="center">{q.designation}</TableCell>
                     <TableCell align="center">{`${status} / ${q.status}`}</TableCell>
                     <TableCell align="center">{`${q.rate_per_day}.00/day`}</TableCell>
@@ -146,34 +157,30 @@ export default function ServiceRecord() {
       </Grid>
 
       <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <Typography align="justify" style={{ textIndent: "5em", margin: "20px 0px 20px 0px", fontSize: "10pt" }}>
+            Issued this {moment().format("Do")} day of {moment().format("MMM")} for whatever legal purposes it may serve.
+          </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={0}>
         <Grid item xs={6} style={{ marginTop: "30px" }}>
-          <Typography>Prepared by:</Typography>
+          <Typography style={{ fontSize: "10pt" }}>Prepared by:</Typography>
           <Typography align="center" style={{ marginTop: "30px" }}>
-            <b>NIDA A. NABUHAY</b>
+            <b>{details.prepby}</b>
           </Typography>
-          <Typography align="center">
-            <i>Administrative Aide II</i>
+          <Typography align="center" style={{ fontSize: "10pt" }}>
+            <i>{details.prepbyPosition}</i>
           </Typography>
         </Grid>
         <Grid item xs={6} style={{ marginTop: "30px" }}>
-          <Typography>Reviewed and Attested by:</Typography>
+          <Typography style={{ fontSize: "10pt" }}>Reviewed and Attested by:</Typography>
           <Typography align="center" style={{ marginTop: "30px" }}>
-            <b>MA. SARINA G. AÑONUEVO</b>
+            <b>{details.approvedby}</b>
           </Typography>
-          <Typography align="center">
-            <i>MGDH I (HRMO V)</i>
-          </Typography>
-        </Grid>
-        <Grid item xs={6}>
-          {/* spacer */}
-        </Grid>
-        <Grid item xs={6} style={isAbsent ? { display: "block", marginTop: "20px" } : { display: "none" }}>
-          <Typography>For and In the Absence of:</Typography>
-          <Typography align="center" style={{ marginTop: "30px" }}>
-            <b>ANNA LOUELLA U. VILLANUEVA</b>
-          </Typography>
-          <Typography align="center">
-            <i>Administrative Officer V (HRMO III)</i>
+          <Typography align="center" style={{ fontSize: "10pt" }}>
+            <i>{details.approvedbyPosition}</i>
           </Typography>
         </Grid>
       </Grid>
@@ -208,7 +215,9 @@ const DialogTitle = withStyles(styles)((props) => {
   );
 });
 
-function FinishingDetails({ open, handleClose, isAbsent, isAbsentChange }) {
+function FinishingDetails({ open, handleClose, details, changes }) {
+  let { prepby, prepbyPosition, approvedby, approvedbyPosition } = details;
+
   return (
     <Dialog
       onClose={handleClose}
@@ -222,7 +231,11 @@ function FinishingDetails({ open, handleClose, isAbsent, isAbsentChange }) {
       </DialogTitle>
       <DialogContent dividers>
         {/* <Alert severity="warning">This page is better printed on Mozilla Firefox</Alert> */}
-        <FormControlLabel control={<Checkbox checked={isAbsent} onChange={isAbsentChange} />} label="Attester is absent" />
+        {/* <FormControlLabel control={<Checkbox checked={isAbsent} onChange={isAbsentChange} />} label="Attester is absent" /> */}
+        <Gtextfield type="text" id="prepby" label="Prepared by" value={prepby} onChange={changes} InputLabelProps={{ shrink: true }} />
+        <Gtextfield type="text" id="prepbyPosition" label="Position" value={prepbyPosition} onChange={changes} InputLabelProps={{ shrink: true }} />
+        <Gtextfield type="text" id="approvedby" label="Approved by" value={approvedby} onChange={changes} InputLabelProps={{ shrink: true }} />
+        <Gtextfield type="text" id="approvedbyPosition" label="Position" value={approvedbyPosition} onChange={changes} InputLabelProps={{ shrink: true }} />
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Print</Button>
